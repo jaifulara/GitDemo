02ba1f126ee0fe091dd0840e700319a53e3703ff
Jaideep Fulara <jaideep.fulara5@gmail.com>
2023-03-04 16:37:19 +0000

    I always forget this step

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -14,7 +14,7 @@ from utilities.BaseClass import BaseClass
 
 
 class TestOne(BaseClass):
-    def test_e2e(self):
+    def test_e2e_Indonesia(self):
         log = self.getLogger()
         homePage = HomePage(self.driver)
         checkOutPage = homePage.shopItems()
@@ -42,22 +42,35 @@ class TestOne(BaseClass):
         print(SuccessText)
         print("The received message from test is", SuccessText)
 
-        print(SuccessText)
-        print("The received message from test is", SuccessText)
-
-        print(SuccessText)
-        print("The received message from test is", SuccessText)
-
-        print(SuccessText)
-        print("The received message from test is", SuccessText)
-
-        print(SuccessText)
-        print("The received message from test is", SuccessText)
-
-        print(SuccessText)
-        print("The received message from test is", SuccessText)
-
-
-        print(SuccessText)
-        print("The received message from test is", SuccessText)
         assert ("Success!" in SuccessText)
+
+
+    def test_e2e_India(self):
+        log = self.getLogger()
+        homePage = HomePage(self.driver)
+        checkOutPage = homePage.shopItems()
+        log.info("Getting all the card titles")
+        cards = checkOutPage.getCardtTitles()
+        i = -1
+        for card in cards:
+            cardText = card.text
+            log.info(cardText)
+            i = i + 1
+            if cardText == "Blackberry":
+                checkOutPage.getCardFooter()[i].click()
+
+        checkOutPage.clickCheckoutBtn().click()
+        confirmPage = checkOutPage.clickFinalCheckout()
+        confirmPage.countryInput().send_keys("Ind")
+        log.info("Entering country name as Ind")
+
+        self.verifyLinkPresence("India")
+        confirmPage.countrySelect().click()
+        confirmPage.check().click()
+        confirmPage.submit().click()
+        SuccessText = confirmPage.success().text
+        print(SuccessText)
+        print("The received message from test is", SuccessText)
+
+        assert ("Success!" in SuccessText)
+
